4ab3944678f23c236989b4fd30e1cac27fff0b34
thomasem <me@thomasem.net>
2024-08-08 12:57:14 -0500

    Added some validation, admin HTML, and minor refactor

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 const (
 	contentTypeHeader    = "Content-Type"
 	textPlainContentType = "text/plain; charset=utf-8"
+	htmlContentType      = "text/html"
+	jsonContentType      = "application/json"
 )
 
 type apiConfig struct {
@@ -16,23 +21,36 @@ type apiConfig struct {
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// This seems like it needs a mutex, but we'll skip for now
+		// This seems like it needs at least a mutex if we have multiple clients, will skip for now
+		// Also, if this gets more complicated than simple incrementing, we may be better off
+		// offloading this to a separate goroutine using a channel to try to reduce overhead
+		// per request
 		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
 	})
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set(contentTypeHeader, textPlainContentType)
+	template := `
+		<html>
+
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+
+		</html>
+	`
+	w.Header().Set(contentTypeHeader, htmlContentType)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits)
+	fmt.Fprintf(w, template, cfg.fileserverHits)
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = 0
 	w.Header().Set(contentTypeHeader, textPlainContentType)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("metrics reset"))
+	w.Write([]byte("Metrics reset!"))
 }
 
 func readyCheck(w http.ResponseWriter, r *http.Request) {
@@ -41,13 +59,85 @@ func readyCheck(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+type chirpRequest struct {
+	Body string `json:"body"`
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type cleanedResponse struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+func cleanBody(body string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(body, " ")
+	for i := range words {
+		for _, bw := range badWords {
+			if strings.ToLower(words[i]) == bw {
+				words[i] = "****"
+				break
+			}
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	cr := chirpRequest{}
+	err := decoder.Decode(&cr)
+	if err != nil {
+		log.Printf("Error decoding chirp request: %s", err)
+		respondWithError(w, http.StatusBadRequest, "invalid JSON request")
+		return
+	}
+	if cr.Body == "" {
+		respondWithError(w, http.StatusBadRequest, "Chirp body missing")
+		return
+	}
+	if len(cr.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleaned := cleanBody(cr.Body)
+	respondWithJSON(w, http.StatusOK, cleanedResponse{CleanedBody: cleaned})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	er := errorResponse{Error: msg}
+	respondWithJSON(w, code, er)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshaling JSON response: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set(contentTypeHeader, jsonContentType)
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
 func configureRoutes(mux *http.ServeMux) {
 	cfg := apiConfig{} // maybe pass in later
+
+	// Admin
+	mux.Handle("GET /admin/metrics", http.HandlerFunc(cfg.metricsHandler))
+
+	// API
+	mux.Handle("GET /api/healthz", http.HandlerFunc(readyCheck))
+	mux.Handle("GET /api/reset", http.HandlerFunc(cfg.resetHandler))
+	mux.Handle("POST /api/validate_chirp", http.HandlerFunc(validateChirpHandler))
+
+	// App
 	appHandler := http.FileServer(http.Dir('.'))
 	mux.Handle("/app/*", http.StripPrefix("/app", cfg.middlewareMetricsInc(appHandler)))
-	mux.Handle("GET /metrics", http.HandlerFunc(cfg.metricsHandler))
-	mux.Handle("/reset", http.HandlerFunc(cfg.resetHandler))
-	mux.Handle("GET /healthz", http.HandlerFunc(readyCheck))
 }
 
 func main() {
@@ -56,12 +146,12 @@ func main() {
 		Handler: mux,
 		Addr:    "localhost:8080",
 	}
-	defer srv.Close()
 
 	configureRoutes(mux)
 
+	log.Printf("Serving on %s", srv.Addr)
 	err := srv.ListenAndServe()
 	if err != nil {
-		fmt.Printf("Server failure: %s\n", err)
+		log.Fatal(err)
 	}
 }
